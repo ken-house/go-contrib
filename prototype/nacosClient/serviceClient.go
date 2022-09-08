7c1a8aaba522502bb9e3e8dc7c8cf75d41e304e0
ken <1136728155@qq.com>
2022-09-08 16:11:48 +0800

    nacos增加获取健康服务ip

--- prototype/nacosClient/serviceClient.go
+++ prototype/nacosClient/serviceClient.go
@@ -1,6 +1,8 @@
 package nacosClient
 
 import (
+	"fmt"
+
 	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
@@ -8,6 +10,7 @@ import (
 
 type ServiceClient interface {
 	naming_client.INamingClient
+	FindHealthInstanceAddress(clusters []string, serviceName string, groupName string) (string, error)
 }
 
 type serviceClient struct {
@@ -32,4 +35,17 @@ func NewServiceClient(cfg Config) (ServiceClient, func(), error) {
 	return &serviceClient{client}, func() {
 		client.CloseClient()
 	}, nil
+}
+
+// FindHealthInstanceAddress 根据serviceId查找到一个健康的服务实例，获取其地址
+func (cli *serviceClient) FindHealthInstanceAddress(clusters []string, serviceName string, groupName string) (string, error) {
+	serviceInfo, err := cli.INamingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
+		Clusters:    clusters,
+		ServiceName: serviceName,
+		GroupName:   groupName,
+	})
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s:%d", serviceInfo.Ip, serviceInfo.Port), nil
 }
